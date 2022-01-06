1453ff847be424b24b77a1999bd523480cf8e328
[Nuno Lopes] <[nunosantoslopes@hotmail.com]>
2022-01-06 02:26:37 +0000

    add personal links and data and the education section

--- scripts/index.js
+++ scripts/index.js
@@ -15,23 +15,3 @@ document.getElementById("whatsapp").addEventListener("click", function(){
     window.open("https://wa.me/+351914555181", '_blank');
 
 }, false);
-
-document.addEventListener("load", function(){
-
-    var http = new XMLHttpRequest();
-
-    http.open("GET", "./json/curriculum.json");
-
-    http.onload = function(){
-
-        if(http.status === 200){
-
-            var cv = JSON.parse(http.response);
-
-        }
-
-    };
-
-    http.send();
-
-}, false);
