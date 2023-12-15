39fbe627782781075628a384499b79369f825bee
MrNoino <nuno.miguelsl25@gmail.com>
2023-12-15 16:57:30 +0000

    Changed the contact links

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,3 @@
-document.getElementById("gmail").addEventListener("click", function(){
-
-    window.open("mailto:nuno.miguelsl25@gmail.com", '_blank');
-
-}, false);
-
-document.getElementById("outlook").addEventListener("click", function(){
-
-    window.open("mailto:nunosantoslopes@hotmail.com", '_blank');
-
-}, false);
-
-document.getElementById("whatsapp").addEventListener("click", function(){
-
-    window.open("https://wa.me/+351914555181", '_blank');
-
-}, false);
-
 var navLinks = document.getElementsByClassName("nav-link");
 
 for(var i = 0; i < navLinks.length; i++){
